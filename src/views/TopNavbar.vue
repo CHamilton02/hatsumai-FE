e8f3c229eeba9bc0e51fe466c57495459f85b905
Camdyn Hamilton <c02hamil@gmail.com>
2025-05-23 21:25:24 -0400

    feat: create top navbar

---
+++ src/views/TopNavbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="top-navbar">
+    <img src="@/assets/hatsumaiLogo.svg" />
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.top-navbar {
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  background-color: #f2efe9;
+}
+</style>
